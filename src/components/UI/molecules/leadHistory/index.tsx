4c15ac78e89cdff2c7fdd26d0a778719555d278b
JorgeSalgado0999 <jorgesalgado_isc@outlook.com>
2023-01-05 12:17:56 -0600

    fixed date type problems

--- src/components/UI/molecules/leadHistory/index.tsx
+++ src/components/UI/molecules/leadHistory/index.tsx
@@ -2,35 +2,39 @@ import {CardHistory} from "@/components/UI/atoms";
 import styles from "./LeadHistory.module.css";
 
 const getLeadContactHistory = () => {
-    return [{
-        type: 'whats',
-        contactDate: new Date(),
-        status: 'Fallido',
-        comments: 'Lorem ipsum dolor sit amet',
-    }, {
-        type: 'phone',
-        status: '???',
-        nextContactDate: new Date(),
-    }];
-}
+	return [
+		{
+			type: "whats",
+			contactDate: new Date(),
+			status: "Fallido",
+			comments: "Lorem ipsum dolor sit amet",
+		},
+		{
+			type: "phone",
+			contactDate: new Date(),
+			status: "???",
+			nextContactDate: new Date(),
+		},
+	];
+};
 
 export const LeadHistory = () => {
-    const leadContactHistory = getLeadContactHistory();
+	const leadContactHistory = getLeadContactHistory();
 
 	return (
 		<div className={styles.tab}>
 			<p className="p3 secondary bold">1er Contacto</p>
-            {
-                leadContactHistory.map(leadContact => {
-                    return <CardHistory
-                        type = { leadContact.type }
-                        contactDate = { leadContact.contactDate }
-                        status = { leadContact.status }
-                        comments = { leadContact.comments }
-                        nextContactDate = { leadContact.nextContactDate }
-                    />
-                })
-            }
+			{leadContactHistory.map((leadContact) => {
+				return (
+					<CardHistory
+						type={leadContact.type}
+						contactDate={leadContact.contactDate}
+						status={leadContact.status}
+						comments={leadContact.comments}
+						nextContactDate={leadContact.nextContactDate}
+					/>
+				);
+			})}
 		</div>
 	);
 };
