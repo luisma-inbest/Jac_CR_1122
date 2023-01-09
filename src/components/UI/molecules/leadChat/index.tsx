b2f2bfb17192d7ef12d434db2f1b75b8a237dfac
JorgeSalgado0999 <jorgesalgado_isc@outlook.com>
2023-01-09 10:03:27 -0600

    .

--- src/components/UI/molecules/leadChat/index.tsx
+++ src/components/UI/molecules/leadChat/index.tsx
@@ -2,25 +2,30 @@ import {CardChat} from "@/components/UI/atoms";
 import styles from "./LeadChat.module.css";
 
 const getLeadContactChat = () => {
-    return [{
-        comments: 'Lorem ipsum dolor sit amet',
-    },];
-}
+	return [
+		{
+			comments: "Lorem ipsum dolor sit amet",
+		},
+	];
+};
 
 export const LeadChat = () => {
 	const leadContactChat = getLeadContactChat();
 
 	return (
 		<div className="row">
-			<div className="col-xs-12" >
-			<span className= "logo"> AG </span>
-				<p className={`bold black`}>Abelardo Garrido</p>
-				<p className={`p3 ${styles.leadTemperature}`}>Ultima cónexion: 10 de oct 2022</p>
+			<div className="col-xs-12">
+				<div style={{width: "100%"}}>
+					<iframe
+						src="https://storage.net-fs.com/hosting/6660567/2/"
+						style={{
+							width: "100%",
+							height: "auto",
+							aspectRatio: "3 / 2",
+						}}
+					></iframe>
+				</div>
 			</div>
-
-
-
 		</div>
-
-	)
+	);
 };
