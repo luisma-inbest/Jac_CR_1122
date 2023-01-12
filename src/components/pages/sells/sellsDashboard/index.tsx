0eaab921e8fcdf25c3ed3e5b198bc13b7638183d
JorgeSalgado0999 <jorgesalgado_isc@outlook.com>
2023-01-12 13:11:54 -0600

    added texts to charts

--- src/components/pages/sells/sellsDashboard/index.tsx
+++ src/components/pages/sells/sellsDashboard/index.tsx
@@ -3,6 +3,7 @@ import {Link} from "react-router-dom";
 import {BarChart, PieChart, StyledInputText} from "@/components/UI/atoms";
 import {Tabs} from "@/components/templates";
 import styles from "./Sells.module.css";
+import {IconSells} from "@/assets";
 
 export const SellsDashboard = () => {
 	return (
@@ -10,9 +11,48 @@ export const SellsDashboard = () => {
 			{/* col */}
 			<div className={` ${styles.cardContainer} ${styles.card1}`}>
 				<div className={`globalPadding ${styles.card} `}>
-					<h1>Leads</h1>
-					<div className={`${styles.chart1}`}>
-						<PieChart />
+					<div className={styles.title}>
+						<div className={styles.iconContainer}>
+							<IconSells size="100%" color="black" />
+						</div>
+						<h1>Leads</h1>
+					</div>
+					<div className="row">
+						<div className={`col-xs-12 col-md-6`}>
+							<div className={`${styles.chart1}`}>
+								<PieChart />
+							</div>
+						</div>
+
+						<div className={`col-xs-12 col-md-6 row`}>
+							<div className={`col-xs-12 col-md-6 ${styles.customColumn}`}>
+								<div className={styles.box}>
+									<p className="p2 bold secondary text-center">
+										Cantidad de Ventas
+									</p>
+									<h1 className="text-center">756</h1>
+								</div>
+								<div className={styles.box}>
+									<p className="p2 bold secondary text-center">Leads</p>
+									<h1 className="text-center">2,924</h1>
+								</div>
+							</div>
+							<div className={`col-xs-12 col-md-6 ${styles.customColumn}`}>
+								<div className={styles.box}>
+									<p className="p2 bold secondary text-center">
+										Cantidad de Ventas
+									</p>
+									<h1 className="text-center">756</h1>
+								</div>
+
+								<div className={styles.box}>
+									<p className="p2 bold secondary text-center">
+										Tiempo de primer contacto
+									</p>
+									<h1 className="text-center">12 mins</h1>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -22,7 +62,12 @@ export const SellsDashboard = () => {
 				className={` ${styles.cardContainer} ${styles.sideBar} ${styles.card2}`}
 			>
 				<div className={`globalPadding ${styles.card}`}>
-					<h1>vehiculos </h1>
+					<div className={styles.title}>
+						<div className={styles.iconContainer}>
+							<IconSells size="100%" color="black" />
+						</div>
+						<h1>vehiculos </h1>
+					</div>
 					{/* <Bar data={chartData} /> */}
 					<BarChart axis="y" />
 				</div>
@@ -31,7 +76,12 @@ export const SellsDashboard = () => {
 			{/* col */}
 			<div className={` ${styles.cardContainer} ${styles.card3}`}>
 				<div className={`globalPadding ${styles.card}`}>
-					<h1>ventas </h1>
+					<div className={styles.title}>
+						<div className={styles.iconContainer}>
+							<IconSells size="100%" color="black" />
+						</div>
+						<h1>ventas </h1>
+					</div>
 					<BarChart axis="x" />
 				</div>
 			</div>
