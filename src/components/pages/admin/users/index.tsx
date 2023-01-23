53433e3f28e5a15511d016de0179a69550afa4c7
Jorge Luis Salgado Ledezma <jorgesalgado_isc@outlook.com>
2023-01-23 11:52:02 -0600

    login merge and changes in user

--- src/components/pages/admin/users/index.tsx
+++ src/components/pages/admin/users/index.tsx
@@ -1,17 +1,16 @@
 import React from "react";
-import { Tabs } from "@/components/templates";
-import { UsersTable } from "@/components/UI/organisms/usersTable";
-import { UserRowProps } from "@/components/UI/molecules/userRow";
-
+import {Tabs} from "@/components/templates";
+import {UsersTable} from "@/components/UI/organisms/usersTable";
+import {UserRowProps} from "@/components/UI/molecules/userRow";
 
 const getUsersMock = () => {
 	const user: UserRowProps = {
-		name: 'Nombre del colaborador',
-		position: 'Puesto del colaborador',
-		area: 'Nombre del área',
-		manager: 'Nombre del gerente',
-		email: 'nombreusuario@oag.com',
-		role: 'Administrador Maestro',
+		name: "Nombre del colaborador",
+		position: "Puesto del colaborador",
+		area: "Nombre del área",
+		manager: "Nombre del gerente",
+		email: "nombreusuario@oag.com",
+		role: "Administrador Maestro",
 	};
 
 	return [user, user, user, user];
@@ -19,21 +18,12 @@ const getUsersMock = () => {
 
 export const Users = () => {
 	const users = getUsersMock();
-	const tabLabels = ['Usuarios', 'Roles'];
-	const tabComponents = [
-		<UsersTable users = {users} />,
-		<div>Aquí van los roles.</div>
-	];
 
 	return (
 		<div className="row">
 			<div className="col-xs">
 				<div className="box">
-					<Tabs
-						tabs={tabLabels}
-						components={tabComponents}
-						full={true}
-					/>
+					<UsersTable users={users} />,
 				</div>
 			</div>
 			{/* <div className="col-xs-6">
