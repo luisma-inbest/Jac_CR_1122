e8c17d53a661ec0260bf7b1fcac22ca1ef4f4236
Arce Gutierrez <arcelia.gutierrez@inbest.cloud>
2023-01-24 09:47:12 -0600

    Vista de usuarios/ vista del botón/Pantallla roles de usuario

--- src/components/UI/molecules/userRow/index.tsx
+++ src/components/UI/molecules/userRow/index.tsx
@@ -11,20 +11,20 @@ export interface UserRowProps {
 
 export const UserRow: React.FunctionComponent<UserRowProps> = (props) => {
     return <tr>
-        <td className={styles.center}>
+        <td className={``}>
             <span className="p3 semi-bold no-margin">{props.name}</span>
             <br />
             <span className="p4 secondary no-margin">{props.position}</span>
         </td>
-        <td className={styles.center}>
+        <td className={``}>
             <span className="p3 semi-bold no-margin">{props.area}</span>
             <br />
             <span className="p4 secondary no-margin">{props.manager}</span>
         </td>
-        <td className={styles.center}>
+        <td className={``}>
             <span className="p4 secondary no-margin">{props.email}</span>
         </td>
-        <td className={styles.center}>
+        <td className={``}>
             <span className="p4 secondary no-margin">{props.role}</span>
         </td>
     </tr>;
