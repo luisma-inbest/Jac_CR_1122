837144a44ef27d5d9a3e69c4baff5400920b9897
Jorge Luis Salgado Ledezma <jorgesalgado_isc@outlook.com>
2023-03-30 11:29:48 -0600

    test

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
-// const Domain = "http://localhost:5173";
-const Domain =
-	"http://jac-crm-front-end-dev.s3-website-us-east-1.amazonaws.com";
+const Domain = "http://localhost:5173";
+// const Domain =
+// 	"http://jac-crm-front-end-dev.s3-website-us-east-1.amazonaws.com";
 
 // const Backend = "http://localhost:3001";
 const Backend = "https://39kvm7kjwh.execute-api.us-east-1.amazonaws.com/";
