598f2f1a527b6b5bc1d4095164c8a0cbd010bb9e
Jorge Luis Salgado Ledezma <jorgesalgado_isc@outlook.com>
2023-04-28 12:01:03 -0600

    pwa iconns v2

--- vite.config.ts
+++ vite.config.ts
@@ -27,12 +27,12 @@ export default defineConfig({
 				theme_color: "#ffffff",
 				icons: [
 					{
-						src: "src/assets/pwa/jacIcon_192.png",
+						src: "jacIcon_192.png",
 						sizes: "192x192",
 						type: "image/png",
 					},
 					{
-						src: "src/assets/pwa/jacIcon_512.png",
+						src: "jacIcon_512.png",
 						sizes: "512x512",
 						type: "image/png",
 					},
