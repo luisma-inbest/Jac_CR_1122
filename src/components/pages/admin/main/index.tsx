62ea68ffb8b1cea8ddf826cd68ffe9e1a69189bc
Jorge Luis Salgado Ledezma <jorgesalgado_isc@outlook.com>
2023-06-20 15:20:04 -0600

    notifications

--- src/components/pages/admin/main/index.tsx
+++ src/components/pages/admin/main/index.tsx
@@ -7,6 +7,43 @@ import { UserAPI } from "@/apis";
 import { User } from "@/models";
 
 export const Admin = () => {
+	const applicationServerKey = encodeURIComponent(
+		"BHBOI4eyr1qScBxCnoh6QYfSpLfI9ymu71q0BiD98-dghHDsZMwTuvG3mmfPHLHe8Lv4wEmw1VAWhbY1CIbF4to"
+	);
+
+	const requestNotificationPermission = () => {
+		Notification.requestPermission().then((permission) => {
+			console.log("permission:", permission);
+			if (permission === "granted") {
+				new Notification("Hola mundo");
+			}
+		});
+	};
+
+	const sendPushNotificationCustom = () => {
+		new Notification("Hola mundo");
+	};
+
+	const sendPushNotification = () => {
+		navigator.serviceWorker.ready.then((registration) => {
+			registration.pushManager
+				.subscribe({
+					userVisibleOnly: true,
+					applicationServerKey: applicationServerKey,
+				})
+				.then((subscription) => {
+					// Send the subscription object to your server
+					// to store it and use it to send push notifications later
+				})
+				.catch((error) => {
+					console.log(
+						"Failed to subscribe to push notifications:",
+						error
+					);
+				});
+		});
+	};
+
 	return (
 		<div className="contentVerticalPadding">
 			<div className="row mb-3">
@@ -15,6 +52,18 @@ export const Admin = () => {
 					<br />
 					<Link to="/admin/agencies">ir a agencias</Link>
 					<hr />
+
+					<button onClick={requestNotificationPermission}>
+						Enable Push Notifications
+					</button>
+					<br />
+					<button onClick={sendPushNotification}>
+						Send Push Notifications
+					</button>
+					<br />
+					<button onClick={sendPushNotificationCustom}>
+						Send Push Notifications
+					</button>
 				</div>
 			</div>
 		</div>
